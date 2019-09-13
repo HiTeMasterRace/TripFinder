aa6bc5913d9f3b75d6615f9283197b3401ba8af3
Antoine <gm.anqo@gmail.com>
2019-09-13 11:15:38 +0200

    add search by types, continent and country

--- api/app/City.php
+++ api/app/City.php
@@ -3,9 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
 
 class City extends Model
 {
+    public function country()
+    {
+        return $this->belongsTo('App\Country');
+    }
+
     public function types()
     {
         return $this->belongsToMany('App\Type', 'city_types');
--- api/app/Country.php
+++ api/app/Country.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Country extends Model
 {
-    //
+    public function continent()
+    {
+        return $this->belongsTo('App\Continent');
+    }
 }
